085d716f93db1a381875fa4b116450aada4d2891
Joel Verhagen <joel.verhagen@gmail.com>
2013-09-27 09:51:00 -0400

    Added initial solution and ODJFS scraper project.

---
+++ OdjfsScraper/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdjfsScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
