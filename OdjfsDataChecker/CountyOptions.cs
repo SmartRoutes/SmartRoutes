2c6b827cb416c8ea4df69120856e87bae5a4abef
Joel Verhagen <joel.verhagen@gmail.com>
2013-11-08 15:55:25 -0500

    "c" for county NAME makes no sense

--- OdjfsDataChecker/CountyOptions.cs
+++ OdjfsDataChecker/CountyOptions.cs
@@ -4,7 +4,7 @@ namespace OdjfsDataChecker
 {
     public class CountyOptions
     {
-        [Option('c',
+        [Option('n',
             Required = true,
             HelpText = "The name of the county to scrape, e.g. \"Hamilton\".")]
         public string Name { get; set; }
