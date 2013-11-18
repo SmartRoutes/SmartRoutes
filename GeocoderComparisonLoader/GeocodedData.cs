f761ac816262a704f93899067385de4e0982b6ad
Joel Verhagen <joel.verhagen@gmail.com>
2013-11-18 18:02:58 -0500

    Added GeocoderComparisonLoader.
    This tool loads data from a geocoded location cache I have written for another project.

---
+++ GeocoderComparisonLoader/GeocodedData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GeocoderComparisonLoader
+{
+    public class GeocodedData : Tuple<double, double, DateTime>
+    {
+        public GeocodedData(double latitude, double longitude, DateTime lastGeocodedOn) : base(latitude, longitude, lastGeocodedOn)
+        {
+        }
+
+        public double Latitude
+        {
+            get { return Item1; }
+        }
+
+        public double Longitude
+        {
+            get { return Item2; }
+        }
+
+        public DateTime LastGeocodedOn
+        {
+            get { return Item3; }
+        }
+    }
+}
