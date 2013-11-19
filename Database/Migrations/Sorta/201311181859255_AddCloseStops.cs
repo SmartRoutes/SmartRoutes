6beed53f761f0ef2fff537fee1d734154832c058
Joel Verhagen <joel.verhagen@gmail.com>
2013-11-19 02:01:23 -0500

    SortaDataChecker: re-parse after adding close stops

--- Database/Migrations/Sorta/201311181859255_AddCloseStops.cs
+++ Database/Migrations/Sorta/201311181859255_AddCloseStops.cs
@@ -18,6 +18,9 @@ namespace Database.Migrations.Sorta
                 .ForeignKey("dbo.Stop", t => t.CloseStopId)
                 .Index(t => t.StopId)
                 .Index(t => t.CloseStopId);
+
+            // after this migration, we need to re-parse
+            Sql("DELETE FROM dbo.Archive");
         }
 
         public override void Down()
@@ -27,6 +30,9 @@ namespace Database.Migrations.Sorta
             DropIndex("dbo.CloseStop", new[] {"CloseStopId"});
             DropIndex("dbo.CloseStop", new[] {"StopId"});
             DropTable("dbo.CloseStop");
+
+            // after this migration, we need to re-parse
+            Sql("DELETE FROM dbo.Archive");
         }
     }
 }
