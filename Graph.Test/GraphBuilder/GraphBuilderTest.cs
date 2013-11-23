118cc093304432cccf50bd1554ca62e6ce798a2f
Joel Verhagen <joel.verhagen@gmail.com>
2013-11-23 13:44:28 -0500

    Graph.Test: initial commit

---
+++ Graph.Test/GraphBuilder/GraphBuilderTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SmartRoutes.Graph.Test.GraphBuilder
+{
+    [TestClass]
+    public class GraphBuilderTest
+    {
+        [TestMethod]
+        public void HappyPath()
+        {
+        }
+    }
+}
