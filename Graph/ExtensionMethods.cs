7dbcb69e3dadb0fab7c5730b8c62ce51cc17f610
brathaj <brathaj@mail.uc.edu>
2013-12-06 15:52:19 -0500

    Graph: Dijkstra's moved to Graph.ExtensionMethods

---
+++ Graph/ExtensionMethods.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartRoutes.Graph.Node;
+using SmartRoutes.Heap;
+
+namespace SmartRoutes.Graph
+{
+    internal enum NodeState
+    {
+        Open, Closed
+    }
+
+    public enum Direction 
+    { 
+        Upwind, Downwind 
+    }
+
+    public class NodeInfo
+    {
+        internal FibHeapHandle<INode, TimeSpan> handle;
+        internal TimeSpan travelTime;
+        internal NodeState state;
+        public INode node;
+        public NodeInfo parent;
+    }
+
+    public static class ExtensionMethods
+    {
+        public static List<NodeInfo> Dijkstras(ISet<INode> StartNodes, Func<INode, bool> GoalCheck, Direction direction)
+        {
+            var Results = new List<NodeInfo>();
+            var SearchInfo = new Dictionary<INode, NodeInfo>();
+            var heap = new FibonacciHeap<INode, TimeSpan>();
+
+            // assign search info to StartNodes and place them in queue
+            foreach (var node in StartNodes)
+            {
+                var nodeInfo = new NodeInfo();
+                nodeInfo.state = NodeState.Closed;
+                nodeInfo.travelTime = new TimeSpan(0);
+                nodeInfo.handle = heap.Insert(node, nodeInfo.travelTime);
+                SearchInfo.Add(node, nodeInfo);
+            }
+
+            while (!heap.Empty())
+            {
+                INode current = heap.DeleteMin();
+
+                // get search info
+                NodeInfo currentInfo = null;
+                if (!SearchInfo.TryGetValue(current, out currentInfo))
+                { 
+                    throw new KeyNotFoundException("Node removed from heap did not have associated search info: ");
+                }
+
+                // check for completion
+                if (GoalCheck(current))
+                {
+                    Results.Add(currentInfo);
+                }
+
+                // loop through neighbors and handle business
+                var Neighbors = (direction == Direction.Upwind) ?
+                    current.UpwindNeighbors : current.DownwindNeighbors;
+
+                foreach (var neighbor in Neighbors)
+                {
+                    NodeInfo neighborInfo = null;
+                    if (!SearchInfo.TryGetValue(neighbor, out neighborInfo))
+                    { 
+                        // node is new, give it search info and place in queue
+                        neighborInfo = new NodeInfo();
+                        neighborInfo.node = neighbor;
+                        neighborInfo.parent = currentInfo;
+                        neighborInfo.state = NodeState.Open;
+                        neighborInfo.travelTime = (direction == Direction.Upwind)
+                            ? currentInfo.travelTime + (neighbor.Time - current.Time)
+                            : currentInfo.travelTime + (current.Time - neighbor.Time);
+                        neighborInfo.handle = heap.Insert(neighbor, neighborInfo.travelTime);
+                    }
+                    else
+                    { 
+                        // neighbor is in queue, check state
+                        if (neighborInfo.state == NodeState.Open)
+                        {
+                            // update neighborInfo if this route is better
+                            TimeSpan newTravelTime = (direction == Direction.Upwind)
+                                ? currentInfo.travelTime + (neighbor.Time - current.Time)
+                                : currentInfo.travelTime + (current.Time - neighbor.Time);
+                            if (newTravelTime < neighborInfo.travelTime)
+                            {
+                                // update search info and update queue for new key
+                                neighborInfo.travelTime = newTravelTime;
+                                neighborInfo.parent = currentInfo;
+                                heap.UpdateKey(neighborInfo.handle, newTravelTime);
+                            }
+                        }
+                    }
+                }
+
+                // and we're done with current
+                currentInfo.state = NodeState.Closed;
+            }
+
+            return Results;
+        }
+    }
+}
--- Graph/Graph.cs
+++ Graph/Graph.cs
@@ -15,37 +15,15 @@ using SmartRoutes.Heap;
 
 namespace SmartRoutes.Graph
 {
-    internal enum NodeState
-    {
-        Open, Closed
-    }
-
-    public enum Direction 
-    { 
-        Upwind, Downwind 
-    }
-
-    public class NodeInfo
-    {
-        internal FibHeapHandle<INode, TimeSpan> handle;
-        internal TimeSpan travelTime;
-        internal NodeState state;
-        public INode node;
-        public NodeInfo parent;
-    }
-
     public class Graph : IGraph
     {
         private readonly IGraphBuilder Builder;
         private EntityCollection Collection;
         private ChildCare[] ChildCares;
-        private Dictionary<INode, NodeInfo> SearchInfo { get; set; }
         public INode[] GraphNodes { get; private set; }
-        public IFibonacciHeap<INode, TimeSpan> Queue { get; set; }
 
         public Graph(IGraphBuilder Builder, IFibonacciHeap<INode, TimeSpan> Queue)
         {
-            this.Queue = Queue;
             this.Builder = Builder;
             GetSortaEntities();
             GetChildCares();
@@ -79,84 +57,6 @@ namespace SmartRoutes.Graph
                 Collection.Services = (from e in ctx.Services select e).ToList();
                 Collection.ContainsEntities = true;
             }
-        }
-
-        public List<NodeInfo> Dijkstras(ISet<INode> StartNodes, Func<INode, bool> GoalCheck, Direction direction)
-        {
-            var Results = new List<NodeInfo>();
-            SearchInfo = new Dictionary<INode, NodeInfo>();
-
-            // assign search info to StartNodes and place them in queue
-            foreach (var node in StartNodes)
-            {
-                var nodeInfo = new NodeInfo();
-                nodeInfo.state = NodeState.Closed;
-                nodeInfo.travelTime = new TimeSpan(0);
-                nodeInfo.handle = Queue.Insert(node, nodeInfo.travelTime);
-                SearchInfo.Add(node, nodeInfo);
-            }
-
-            while (!Queue.Empty())
-            {
-                INode current = Queue.DeleteMin();
-
-                // get search info
-                NodeInfo currentInfo = null;
-                if (!SearchInfo.TryGetValue(current, out currentInfo))
-                { 
-                    throw new KeyNotFoundException("Node removed from heap did not have associated search info: ");
-                }
-
-                // check for completion
-                if (GoalCheck(current))
-                {
-                    Results.Add(currentInfo);
-                }
-
-                // loop through neighbors and handle business
-                var Neighbors = (direction == Direction.Upwind) ?
-                    current.UpwindNeighbors : current.DownwindNeighbors;
-
-                foreach (var neighbor in Neighbors)
-                {
-                    NodeInfo neighborInfo = null;
-                    if (!SearchInfo.TryGetValue(neighbor, out neighborInfo))
-                    { 
-                        // node is new, give it search info and place in queue
-                        neighborInfo = new NodeInfo();
-                        neighborInfo.node = neighbor;
-                        neighborInfo.parent = currentInfo;
-                        neighborInfo.state = NodeState.Open;
-                        neighborInfo.travelTime = (direction == Direction.Upwind)
-                            ? currentInfo.travelTime + (neighbor.Time - current.Time)
-                            : currentInfo.travelTime + (current.Time - neighbor.Time);
-                        neighborInfo.handle = Queue.Insert(neighbor, neighborInfo.travelTime);
-                    }
-                    else
-                    { 
-                        // neighbor is in queue, check state
-                        if (neighborInfo.state == NodeState.Open)
-                        {
-                            // update neighborInfo if this route is better
-                            TimeSpan newTravelTime = (direction == Direction.Upwind)
-                                ? currentInfo.travelTime + (neighbor.Time - current.Time)
-                                : currentInfo.travelTime + (current.Time - neighbor.Time);
-                            if (newTravelTime < neighborInfo.travelTime)
-                            {
-                                // update search info and update queue for new key
-                                neighborInfo.travelTime = newTravelTime;
-                                neighborInfo.parent = currentInfo;
-                                Queue.UpdateKey(neighborInfo.handle, newTravelTime);
-                            }
-                        }
-                    }
-                }
-
-                // and we're done with current
-                currentInfo.state = NodeState.Closed;
-            }
-
-            return Results;
         }
     }
 }
