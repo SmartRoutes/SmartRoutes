504643e50227d998c770981f14b0f353ef0ee9d7
brathaj <brathaj@mail.uc.edu>
2013-12-06 16:48:27 -0500

    Added test project for Dijkstras

---
+++ Dijkstras/DijkstrasTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartRoutes.Graph.Node;
+
+namespace SmartRoutes.Graph.Test
+{
+    [TestClass]
+    public class DijkstrasTest
+    {
+        [TestMethod]
+        public void TestTwoNodeGraph()
+        {
+            var graph = GraphCreationMethods.TwoNodeGraph();
+            var StartNodes = new INode[] { graph.GraphNodes[0] };
+            var result = SmartRoutes.Graph.ExtensionMethods.Dijkstras(
+                StartNodes,
+                x => x.Name == "B",
+                SmartRoutes.Graph.Direction.Upwind);
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual("B", result[0].node.Name);
+            Assert.AreEqual("A", result[0].parent.node.Name);
+            Assert.IsNull(result[0].parent.parent);
+        }
+    }
+}
---
+++ Dijkstras/GraphTestImplementation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartRoutes.Graph.Node;
+
+namespace SmartRoutes.Graph.Test
+{
+    public class GraphTestImplementation : IGraph
+    {
+        public void GetSortaEntities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetChildCares()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Node.INode[] GraphNodes { get; set; }
+    }
+
+    class NodeTestImplementation : INode
+    {
+        public ISet<INode> UpwindNeighbors { get; set; }
+        public ISet<INode> DownwindNeighbors { get; set; }
+        public DateTime Time { get; set; }
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public NodeTestImplementation(string Name, DateTime Time)
+        {
+            DownwindNeighbors = new HashSet<INode>();
+            UpwindNeighbors = new HashSet<INode>();
+            this.Name = Name;
+            this.Time = Time;
+        }
+    }
+
+    static class GraphCreationMethods
+    {
+        public static GraphTestImplementation TwoNodeGraph()
+        {
+            var NodeA = new NodeTestImplementation("A", new DateTime(1970, 1, 1, 10, 0, 0));
+            var NodeB = new NodeTestImplementation("B", new DateTime(1970, 1, 1, 12, 0, 0));
+
+            NodeA.UpwindNeighbors.Add(NodeB);
+            NodeB.DownwindNeighbors.Add(NodeA);
+
+            var graph = new GraphTestImplementation();
+            graph.GraphNodes = new INode[] { NodeA, NodeB };
+
+            return graph;
+        }
+    }
+}
