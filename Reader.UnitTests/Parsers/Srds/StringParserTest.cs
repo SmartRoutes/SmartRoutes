9d85886f8bc5aeec5ffd4605374dcffa3a9c96b3
Joel Verhagen <joel.verhagen@gmail.com>
2013-12-27 17:26:14 -0500

    Add nullable type support to StringParser
    Add support for bool, int, double, float, and DateTime

--- Reader.UnitTests/Parsers/Srds/StringParserTest.cs
+++ Reader.UnitTests/Parsers/Srds/StringParserTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SmartRoutes.Reader.Parsers.Srds;
 
@@ -7,20 +9,29 @@ namespace SmartRoutes.Reader.UnitTests.Parsers.Srds
     [TestClass]
     public class StringParserTest
     {
+        private static readonly ISet<string> SupportedClasses = new HashSet<string>
+        {
+            "System.String"
+        };
+
+        private static readonly ISet<string> SupporedStructs = new HashSet<string>
+        {
+            "System.Boolean", "System.DateTime", "System.Int32", "System.Double", "System.Single"
+        };
+
         [TestMethod]
-        public void Parse_String()
+        public void Parse_Valid()
         {
             // ARRANGE
             var parser = new StringParser();
-            const string expected = "Foo";
 
             // ACT
-            object output = parser.Parse("System.String", expected);
+            object output = parser.Parse("System.String", "Foo");
 
             // ASSERT
             var actual = output as string;
             Assert.IsNotNull(actual);
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual("Foo", actual);
         }
 
         [TestMethod]
@@ -32,7 +43,7 @@ namespace SmartRoutes.Reader.UnitTests.Parsers.Srds
             // ACT
             try
             {
-                parser.Parse("Voynich.Manuscript", "foo");
+                parser.Parse("Voynich.Manuscript", "Foo");
 
                 // ASSERT
                 Assert.Fail();
@@ -43,13 +54,78 @@ namespace SmartRoutes.Reader.UnitTests.Parsers.Srds
         }
 
         [TestMethod]
-        public void SupportsParsing_True_String()
+        public void Parse_Invalid()
+        {
+            // ARRANGE
+            var parser = new StringParser();
+
+            // ACT
+            try
+            {
+                parser.Parse("System.Int32", "Foo");
+
+                // ASSERT
+                Assert.Fail();
+            }
+            catch (FormatException)
+            {
+            }
+        }
+
+        [TestMethod]
+        public void Parse_Nullable_WithValue()
+        {
+            // ARRANGE
+            var parser = new StringParser();
+
+            // ACT
+            object output = parser.Parse("System.Int32?", "23");
+
+            // ASSERT
+            var actual = output as int?;
+            Assert.IsNotNull(actual);
+            Assert.IsTrue(actual.HasValue);
+            Assert.AreEqual(23, actual.Value);
+        }
+
+        [TestMethod]
+        public void Parse_Nullable_WithoutValue_All()
         {
             // ARRANGE
             var parser = new StringParser();
 
             // ACT, ASSERT
-            Assert.IsTrue(parser.SupportsParsing("System.String"));
+            foreach (string type in SupporedStructs)
+            {
+                object output = parser.Parse(type + "?", string.Empty);
+                Assert.IsNull(output);
+            }
+        }
+
+        [TestMethod]
+        public void SupportsParsing_True_All()
+        {
+            // ARRANGE
+            var parser = new StringParser();
+
+            // ACT, ASSERT
+            foreach (string type in SupporedStructs.Concat(SupportedClasses))
+            {
+                Assert.IsTrue(parser.SupportsParsing(type), string.Format("Unsupported type: {0}", type));
+            }
+        }
+
+        [TestMethod]
+        public void SupportsParsing_True_Nullable()
+        {
+            // ARRANGE
+            var parser = new StringParser();
+
+            // ACT, ASSERT
+            foreach (string type in SupporedStructs)
+            {
+                Assert.IsTrue(parser.SupportsParsing(type + "?"), string.Format("Unsupported type: {0}?", type));
+            }
         }
 
         [TestMethod]
--- Reader/Parsers/Srds/StringParser.cs
+++ Reader/Parsers/Srds/StringParser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using NLog;
@@ -9,26 +10,35 @@ namespace SmartRoutes.Reader.Parsers.Srds
     public class StringParser : IStringParser
     {
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
-        private static readonly IDictionary<Type, Func<string, object>> StringParsers;
+        private static readonly IDictionary<Type, Func<string, object>> TypeToStringParser;
+        private static readonly IDictionary<Type, Func<string, object>> TypeToNullableStringParser;
 
+        private readonly IDictionary<string, Func<string, object>> _typeNameToStringParser = new Dictionary<string, Func<string, object>>();
         private readonly IDictionary<string, Type> _types = new Dictionary<string, Type>();
 
         static StringParser()
         {
             // initialize the dictionary of different string parsers
             Type thisType = typeof (StringParser);
-            MethodInfo[] stringParsers = thisType.GetMethods(BindingFlags.Static | BindingFlags.NonPublic)
+            MethodInfo[] stringParserInfos = thisType.GetMethods(BindingFlags.Static | BindingFlags.NonPublic)
                 .Where(m =>
                     m.Name.StartsWith("Parse") &&
                     m.GetParameters().Length == 1 &&
                     m.GetParameters().First().ParameterType == typeof (string))
                 .ToArray();
 
-            StringParsers = new Dictionary<Type, Func<string, object>>();
-            foreach (MethodInfo stringParser in stringParsers)
+            TypeToStringParser = new Dictionary<Type, Func<string, object>>();
+            TypeToNullableStringParser = new Dictionary<Type, Func<string, object>>();
+            foreach (MethodInfo info in stringParserInfos)
             {
-                MethodInfo localStringParser = stringParser;
-                StringParsers[stringParser.ReturnType] = s => localStringParser.Invoke(null, new object[] {s});
+                MethodInfo localInfo = info;
+                Func<string, object> stringParser = s => Parse(localInfo, s);
+                TypeToStringParser[info.ReturnType] = stringParser;
+                if (info.ReturnType.IsValueType)
+                {
+                    Func<string, object> nullableStringParser = s => ParseNullable(stringParser, s);
+                    TypeToNullableStringParser[info.ReturnType] = nullableStringParser;
+                }
             }
         }
 
@@ -50,33 +60,126 @@ namespace SmartRoutes.Reader.Parsers.Srds
             return GetStringParser(typeName)(value);
         }
 
+        private static object Parse(MethodInfo info, string value)
+        {
+            try
+            {
+                return info.Invoke(null, new object[] {value});
+            }
+            catch (TargetInvocationException e)
+            {
+                if (e.InnerException != null)
+                {
+                    throw e.InnerException;
+                }
+                throw;
+            }
+        }
+
+        private static object ParseNullable(Func<string, object> stringParser, string value)
+        {
+            if (value == string.Empty)
+            {
+                return null;
+            }
+            return stringParser(value);
+        }
+
         private Func<string, object> GetStringParser(string typeName)
         {
-            // get the type
-            Type type;
-            if (!_types.ContainsKey(typeName))
+            // check the cache
+            if (_typeNameToStringParser.ContainsKey(typeName))
             {
-                type = Type.GetType(typeName);
-                _types[typeName] = type;
+                return _typeNameToStringParser[typeName];
+            }
+
+            // check for nullable flag
+            bool nullable = typeName.EndsWith("?");
+            string underlyingTypeName;
+            if (nullable)
+            {
+                underlyingTypeName = typeName.Substring(0, typeName.Length - 1);
             }
             else
             {
-                type = _types[typeName];
+                underlyingTypeName = typeName;
+            }
+
+            // get the type
+            Type type;
+            if (!_types.ContainsKey(underlyingTypeName))
+            {
+                type = Type.GetType(underlyingTypeName);
+                _types[underlyingTypeName] = type;
+            }
+            else
+            {
+                type = _types[underlyingTypeName];
+            }
+            if (type == null)
+            {
+                var exception = new ArgumentException("The provided type name is not valid.");
+                Logger.ErrorException(string.Format("TypeName: {0}", underlyingTypeName), exception);
+                throw exception;
             }
 
             // get the parser
-            if (!StringParsers.ContainsKey(type))
+            Func<string, object> stringParser;
+            if (nullable)
             {
-                var exception = new ArgumentException("The provided type name does not have an associated string parser.");
-                Logger.ErrorException(string.Format("TypeName: {0}", typeName), exception);
-                throw exception;
+                if (!TypeToNullableStringParser.TryGetValue(type, out stringParser))
+                {
+                    ThrowUnsupportedType(typeName);
+                }
             }
-            return StringParsers[type];
+            else
+            {
+                if (!TypeToStringParser.TryGetValue(type, out stringParser))
+                {
+                    ThrowUnsupportedType(typeName);
+                }
+            }
+
+            // cache the result
+            _typeNameToStringParser[typeName] = stringParser;
+            return stringParser;
+        }
+
+        private static void ThrowUnsupportedType(string typeName)
+        {
+            var exception = new ArgumentException("The provided type name does not have an associated string parser.");
+            Logger.ErrorException(string.Format("TypeName: {0}", typeName), exception);
+            throw exception;
         }
 
         private static string ParseString(string value)
         {
             return value;
+        }
+
+        private static bool ParseBoolean(string value)
+        {
+            return Convert.ToBoolean(value);
+        }
+
+        private static int ParseInt32(string value)
+        {
+            return Convert.ToInt32(value);
+        }
+
+        private static double ParseDouble(string value)
+        {
+            return Convert.ToDouble(value);
+        }
+
+        private static float ParseFloat(string value)
+        {
+            return Convert.ToSingle(value);
+        }
+
+        private static DateTime ParseDateTime(string value)
+        {
+            return DateTime.ParseExact(value, "R", CultureInfo.InvariantCulture);
         }
     }
 }
