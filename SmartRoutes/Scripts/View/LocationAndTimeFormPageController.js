5a61de3733673381330c3f5c9798bf5ab1860aeb
smithb589 <smithb589@gmail.com>
2014-01-20 14:24:47 -0500

    Setting up bindings for the locations and times form page.

--- SmartRoutes/Scripts/View/LocationAndTimeFormPageController.js
+++ SmartRoutes/Scripts/View/LocationAndTimeFormPageController.js
@@ -3,13 +3,24 @@ SmartRoutes.LocationAndTimeFormPageController = function(formPageRouteMap) {
     // Private:
 
     var pageIDRouteMap = formPageRouteMap;
+    var locationAndTimeViewModel = null;
 
     (function Init() {
+        locationAndTimeViewModel = new SmartRoutes.LocationAndTimeViewModel();
 
+        ko.applyBindings(locationAndTimeViewModel, $("#sr-location-time-form-page-view"));
 
+        ko.applyBindings(locationAndTimeViewModel.pickUpDepartAddress, $("#sr-section-drop-off-depart"));
+        ko.applyBindings(locationAndTimeViewModel.pickupDestinationAddress, $("#sr-section-drop-off-final-destination"));
+        ko.applyBindings(locationAndTimeViewModel.dropOffDepartAddress, $("#sr-section-pick-up-depart"));
+        ko.applyBindings(locationAndTimeViewModel.dropOffDestinationAddress, $("#sr-section-pick-up-final-destination"));
     })();
 
     return {
         // Public: 
+
+        GetLocationAndTimeViewModel: function() {
+            return locationAndTimeViewModel;
+        }
     };
 };
