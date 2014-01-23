d19ecfe98a9b81b8ed653aef1159332bcdc7e5fb
smithb589 <smithb589@gmail.com>
2014-01-23 10:07:33 -0500

    Renaming the javascript models folder to viewmodels to reflect what it actually contains.  Also removing an unused script render from the layout page.

--- SmartRoutes/App_Start/BundleConfig.cs
+++ SmartRoutes/App_Start/BundleConfig.cs
@@ -27,16 +27,16 @@ namespace SmartRoutes
                         "~/Scripts/View/SmartRoutes.js",
                         
                         // View Models.
-                        "~/Scripts/Models/ChildInfoViewModel.js",
-                        "~/Scripts/Models/ScheduleTypeViewModel.js",
-                        "~/Scripts/Models/AddressViewModel.js",
-                        "~/Scripts/Models/DropOffDepartureViewModel.js",
-                        "~/Scripts/Models/DropOffDestinationViewModel.js",
-                        "~/Scripts/Models/PickUpDepartureViewModel.js",
-                        "~/Scripts/Models/PickUpDestinationViewModel.js",
-                        "~/Scripts/Models/LocationAndTimeViewModel.js",
-                        "~/Scripts/Models/AccreditationViewModel.js",
-                        "~/Scripts/Models/ServiceTypeViewModel.js",
+                        "~/Scripts/ViewModels/ChildInfoViewModel.js",
+                        "~/Scripts/ViewModels/ScheduleTypeViewModel.js",
+                        "~/Scripts/ViewModels/AddressViewModel.js",
+                        "~/Scripts/ViewModels/DropOffDepartureViewModel.js",
+                        "~/Scripts/ViewModels/DropOffDestinationViewModel.js",
+                        "~/Scripts/ViewModels/PickUpDepartureViewModel.js",
+                        "~/Scripts/ViewModels/PickUpDestinationViewModel.js",
+                        "~/Scripts/ViewModels/LocationAndTimeViewModel.js",
+                        "~/Scripts/ViewModels/AccreditationViewModel.js",
+                        "~/Scripts/ViewModels/ServiceTypeViewModel.js",
 
                         // Common controllers.
                         "~/Scripts/View/PortalViewController.js",
@@ -61,32 +61,6 @@ namespace SmartRoutes
                         "~/Content/Views/MainPageView.css",
                         "~/Content/Views/PortalView.css",
                         "~/Content/Views/GuidedSearchPageView.css"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-            //            "~/Scripts/jquery-ui-{version}.js"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //            "~/Scripts/jquery.unobtrusive*",
-            //            "~/Scripts/jquery.validate*"));
-
-            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
-            //// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/Scripts/modernizr-*"));
-
-            //bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-            //            "~/Content/themes/base/jquery.ui.core.css",
-            //            "~/Content/themes/base/jquery.ui.resizable.css",
-            //            "~/Content/themes/base/jquery.ui.selectable.css",
-            //            "~/Content/themes/base/jquery.ui.accordion.css",
-            //            "~/Content/themes/base/jquery.ui.autocomplete.css",
-            //            "~/Content/themes/base/jquery.ui.button.css",
-            //            "~/Content/themes/base/jquery.ui.dialog.css",
-            //            "~/Content/themes/base/jquery.ui.slider.css",
-            //            "~/Content/themes/base/jquery.ui.tabs.css",
-            //            "~/Content/themes/base/jquery.ui.datepicker.css",
-            //            "~/Content/themes/base/jquery.ui.progressbar.css",
-            //            "~/Content/themes/base/jquery.ui.theme.css"));
         }
     }
 }
