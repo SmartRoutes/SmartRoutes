ebd8aa8fd384e8b7320a93afe96185e17e916a2d
brathaj <brathaj@mail.uc.edu>
2014-01-26 10:15:56 -0500

    Updated IGraph to match previous commit

--- Graph/IGraph.cs
+++ Graph/IGraph.cs
@@ -13,8 +13,7 @@ namespace SmartRoutes.Graph
     public interface IGraph
     {
         INode[] GraphNodes { get; }
-        IEnumerable<Stop> GetClosestGtfsStops(ILocation location, int numStops);
-        IEnumerable<IGtfsNode> GetClosestGtfsNodes(ILocation location, DateTime time, TimeDirection direction, int numNodes);
+        IEnumerable<IGtfsNode> GetClosestGtfsNodes(ILocation location, DateTime time, TimeDirection direction);
         IEnumerable<IGtfsNode> GetDestinationNeighbors(IDestinationNode destinationNode, TimeDirection direction);
     }
 }
