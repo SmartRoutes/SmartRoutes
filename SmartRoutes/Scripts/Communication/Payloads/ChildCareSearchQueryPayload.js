50e33657d9622eab57e4700f1aa3ec83cf2f2ebc
smithb589 <smithb589@gmail.com>
2014-02-05 13:13:08 -0500

    Adding the search request to the guided search communication controller

--- SmartRoutes/Scripts/Communication/Payloads/ChildCareSearchQueryPayload.js
+++ SmartRoutes/Scripts/Communication/Payloads/ChildCareSearchQueryPayload.js
@@ -1,9 +1,14 @@
 ﻿
 
-SmartRoutes.Communication.ChildCareSearchQueryPayload = function(childInformationPayload, scheduleTypePayload) {
+// The payload object that represents all the data
+// that must be passed to the server to perform
+// a child care search.
+SmartRoutes.Communication.ChildCareSearchQueryPayload = function(childInformationPayload, scheduleTypePayloadArray, accreditationPayloadArray, serviceTypePayload) {
 
     return {
         childInformation: childInformationPayload,
-        scheduleType: scheduleTypePayload
+        scheduleType: scheduleTypePayload,
+        accreditations: accreditationPayloadArray,
+        serviceTypes: serviceTypePayload
     };
 };
