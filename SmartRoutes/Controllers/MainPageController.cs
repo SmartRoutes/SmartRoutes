d9b6ae8faac59e2590d696d965d8e5b61fb2156c
smithb589 <smithb589@gmail.com>
2014-02-12 11:08:04 -0500

    Adding comments to the MVC controller files.

--- SmartRoutes/Controllers/MainPageController.cs
+++ SmartRoutes/Controllers/MainPageController.cs
@@ -7,13 +7,24 @@ using SmartRoutes.Models;
 
 namespace SmartRoutes.Controllers
 {
+    /// <summary>
+    /// This controller handles requests from the main page.
+    /// </summary>
     public class MainPageController : Controller
     {
+        /// <summary>
+        /// Gets the content view.
+        /// </summary>
+        /// <returns>The content view HTML.</returns>
         public ActionResult Index()
         {
             return View("ContentView");
         }
 
+        /// <summary>
+        /// Gets the website description for the main page.
+        /// </summary>
+        /// <returns>A view that is prepopulated with information about the website.</returns>
         public ActionResult DescriptionView()
         {
             SummaryViewModel summaryModel = new SummaryViewModel(Resources.descriptionTitleText,
@@ -22,6 +33,10 @@ namespace SmartRoutes.Controllers
             return PartialView("_SummaryView", summaryModel);
         }
 
+        /// <summary>
+        /// Gets a sample map to display on the main page.
+        /// </summary>
+        /// <returns>A sample map images to display on the page.</returns>
         public ActionResult SampleMapView()
         {
             ImageViewModel sampleMapModel = new ImageViewModel("Content/Images/sample_map.png",
@@ -31,6 +46,10 @@ namespace SmartRoutes.Controllers
             return PartialView("_ImageView", sampleMapModel);
         }
 
+        /// <summary>
+        /// Gets the portal view to display on the left-hand side of the main page.
+        /// </summary>
+        /// <returns>The HTML for the portal view.</returns>
         public ActionResult PortalView()
         {
             PortalViewModel portalViewModel = new PortalViewModel("sr-portal-view-main-page");
--- SmartRoutes/Controllers/ResultsPageController.cs
+++ SmartRoutes/Controllers/ResultsPageController.cs
@@ -6,11 +6,18 @@ using System.Web.Mvc;
 
 namespace SmartRoutes.Controllers
 {
+    /// <summary>
+    /// Controller that handles requests for the results page.
+    /// </summary>
     public class ResultsPageController : Controller
     {
         //
         // GET: /ResultsPage/
 
+        /// <summary>
+        /// Gets the raw HTML for the result view.
+        /// </summary>
+        /// <returns>The HTML for the result view.</returns>
         public ActionResult ResultViewHtml()
         {
             return PartialView("~/Views/Results/_ResultView.cshtml");
