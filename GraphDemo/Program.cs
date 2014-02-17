ae468bb931b208aba8ac5ba069a0a5e51b94774d
Joel Verhagen <joel.verhagen@gmail.com>
2014-02-17 12:26:59 -0500

    Move Graph demo to new project (GraphDemo)

---
+++ GraphDemo/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Linq;
+using Ninject;
+using Ninject.Extensions.Conventions;
+using SmartRoutes.Demo.OdjfsDatabase;
+using SmartRoutes.Graph;
+using SmartRoutes.Graph.Node;
+using SmartRoutes.Model;
+using SmartRoutes.Model.Gtfs;
+using SmartRoutes.Model.Srds;
+using SmartRoutes.Reader.Readers;
+
+namespace SmartRoutes.GraphDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IKernel kernel = new StandardKernel(new GraphModule());
+
+                kernel.Bind(c => c
+                    .FromAssemblyContaining(typeof(GtfsCollection), typeof(IEntityCollectionDownloader<,>))
+                    .SelectAllClasses()
+                    .BindAllInterfaces());
+
+                /********************************************************************************************
+                ******************* Fetch the data from the database and build the graph ********************
+                ********************************************************************************************/
+
+                Console.WriteLine("Fetching the SORTA data from the web.");
+                DateTime tic = DateTime.Now;
+                var gtfsFetcher = kernel.Get<IEntityCollectionDownloader<GtfsArchive, GtfsCollection>>();
+                var gtfsCollection = gtfsFetcher.Download(new Uri("http://www.go-metro.com/uploads/GTFS/google_transit_info.zip"), null).Result;
+                DateTime toc = DateTime.Now;
+                Console.WriteLine("GTFS data fetched in {0} milliseconds.", (toc - tic).TotalMilliseconds);
+
+                Console.WriteLine("Fetching the ODJFS data from the database.");
+                tic = DateTime.Now;
+                var odjfsDatabase = new OdjfsDatabase("OdjfsDatabase");
+                var childCares = odjfsDatabase.GetChildCares().Result;
+                toc = DateTime.Now;
+                Console.WriteLine("Destination data fetched in {0} milliseconds.", (toc - tic).TotalMilliseconds);
+
+                Console.WriteLine("Creating Graph.");
+                tic = DateTime.Now;
+                var graphBuilder = kernel.Get<IGraphBuilder>();
+                var graph = graphBuilder.BuildGraph(gtfsCollection.StopTimes, childCares, GraphBuilderSettings.Default);
+                toc = DateTime.Now;
+                Console.WriteLine("Graph created in {0} milliseconds.", (toc - tic).TotalMilliseconds);
+                Console.WriteLine("Finding route...");
+                tic = DateTime.Now;
+
+                /********************************************************************************************
+                ******************** Example usage of Dijkstras for complete route **************************
+                ********************************************************************************************/
+
+                // starting at my address
+                var homeLocation = new Destination { Latitude = 39.122309, Longitude = -84.507639 };
+
+                // ending at the college of engineering
+                var workLocation = new Destination { Latitude = 39.133292, Longitude = -84.515099 };
+
+                // have to be at work by 10:30 am
+                var atWorkBy = new DateTime(1970, 1, 1, 10, 30, 0);
+
+                // child care selected by name, since properties are not implemented yet
+                const string childCareName1 = "ANOINTED HANDS LEARNING CENTER";
+                const string childCareName2 = "DIVINE DAY CARE CENTER, INC. II";
+
+                Func<IDestination, bool> criteria1 = dest => dest.Name == childCareName1;
+                Func<IDestination, bool> criteria2 = dest => dest.Name == childCareName2;
+
+                var criterion = new[] { criteria1, criteria2 };
+
+                var results = graph.Search(workLocation, homeLocation, atWorkBy, TimeDirection.Backwards, criterion);
+
+                toc = DateTime.Now;
+                Console.WriteLine("Route found in in {0} milliseconds.",
+                    (toc - tic).TotalMilliseconds);
+
+                var current = results.First();
+                Console.WriteLine("Displaying Route.");
+                while (current != null)
+                {
+                    var gtfsCurrent = current.node as IGtfsNode;
+                    if (gtfsCurrent != null)
+                    {
+                        Console.WriteLine("{0} -- {1} <Trip {2}> <Route {3}>", current.node.Name, current.node.Time, gtfsCurrent.TripId, gtfsCurrent.stopTime.Trip.Route.ShortName);
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0} -- {1}", current.node.Name, current.node.Time);
+                    }
+                    current = current.parent;
+                }
+
+                Console.WriteLine("fin");
+
+
+
+                //DateTime toc = DateTime.Now;
+
+                //Console.WriteLine("Graph created in {0} milliseconds.", (toc - tic).TotalMilliseconds);
+                //Console.WriteLine("Performing Dijkstras...");
+                //tic = DateTime.Now;
+
+                //int count = 0;
+
+                //Func<INode, bool> GoalCheck = node =>
+                //{
+                //    var check = node as DestinationNode;
+                //    return check != null;
+                //};
+
+                //var Results = new List<NodeInfo>();
+
+                //var StartNodes = new INode[] { graph.GraphNodes[count] };
+                //Results = ExtensionMethods.Dijkstras(StartNodes, GoalCheck, TimeDirection.Forwards);
+
+                //toc = DateTime.Now;
+                //Console.WriteLine("Dijkstra's completed in {0} milliseconds, {1} results found.", 
+                //    (toc - tic).TotalMilliseconds, Results.Count());
+
+                //List<NodeBase> UniqueChildCareBases = new List<NodeBase>();
+
+                //foreach (var node in graph.GraphNodes)
+                //{
+                //    var cnode = node as DestinationNode;
+                //    if (cnode == null) continue;
+                //    if (!UniqueChildCareBases.Contains(cnode.BaseNode))
+                //    {
+                //        UniqueChildCareBases.Add(cnode.BaseNode);
+                //    }
+                //}
+
+                //Console.WriteLine("{0} unique child cares found in graph.", UniqueChildCareBases.Count());
+
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
