a5ab64bc5aea4a23fb09e4e97abf244e5bb25179
Joel Verhagen <joel.verhagen@gmail.com>
2014-03-06 15:52:31 -0500

    Make IDestination available from IDestinationNode

--- Graph/Node/IDestinationNode.cs
+++ Graph/Node/IDestinationNode.cs
@@ -1,6 +1,10 @@
-﻿namespace SmartRoutes.Graph.Node
+﻿using System.Security.Cryptography.X509Certificates;
+using SmartRoutes.Model;
+
+namespace SmartRoutes.Graph.Node
 {
     public interface IDestinationNode : INode
     {
+        IDestination Destination { get; } 
     }
 }
