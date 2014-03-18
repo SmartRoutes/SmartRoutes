a5b8cf8b1155b8e2ac799df173f6f5fab4520b74
brathaj <brathaj@mail.uc.edu>
2014-03-18 10:35:15 -0400

    DestinationCollectionComparer: Now recognizes equality between collections which may be ordered differently

--- Graph/DestinationCollectionComparer.cs
+++ Graph/DestinationCollectionComparer.cs
@@ -7,18 +7,56 @@ using SmartRoutes.Model;
 
 namespace SmartRoutes.Graph
 {
+    // equality comparer for type IEnumerable<IDestination>
+    // 
     public class DestinationCollectionComparer : IEqualityComparer<IEnumerable<IDestination>>
     {
         public bool Equals(IEnumerable<IDestination> x, IEnumerable<IDestination> y)
         {
-            return x.SequenceEqual(y);
+            bool areEqual = true;
+
+            if (x.Count() != y.Count())
+            {
+                areEqual = false;
+            }
+            else if (areEqual)
+            {
+                foreach (var dest in x)
+                {
+                    if (!y.Contains(dest))
+                    {
+                        areEqual = false;
+                        break;
+                    }
+                }
+            }
+            else if (areEqual)
+            {
+                foreach (var dest in y)
+                {
+                    if (!x.Contains(dest))
+                    {
+                        areEqual = false;
+                        break;
+                    }
+                }
+            }
+
+            return areEqual;
         }
 
         public int GetHashCode(IEnumerable<IDestination> obj)
         {
             if (obj.Count() > 0)
             {
-                return obj.First().GetHashCode();
+                int hash = 17;
+
+                foreach (var dest in obj)
+                {
+                    hash = 23 * dest.GetHashCode();
+                }
+
+                return hash;
             }
             else
             {
