6b968479a17fd4ef6afa3e2769f557487673ffcc
Joel Verhagen <joel.verhagen@gmail.com>
2014-03-21 03:44:20 -0400

    Removed unused map models

--- SmartRoutes/Models/Map/StopTimeLocationModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SmartRoutes.Models.Map
-{
-    public class StopTimeLocationModel : TimeLocationModel
-    {
-        public string Route { get; set; }
-        public string Stop { get; set; }
-    }
-}
