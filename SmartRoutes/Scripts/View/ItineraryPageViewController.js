dc5d646057aef4c58b3ae77266e43ce94f82b68b
Joel Verhagen <joel.verhagen@gmail.com>
2014-03-24 16:47:55 -0400

    Add maps to itinerary page

--- SmartRoutes/Scripts/View/ItineraryPageViewController.js
+++ SmartRoutes/Scripts/View/ItineraryPageViewController.js
@@ -13,9 +13,18 @@ SmartRoutes.ItineraryPageViewController = function(pageID) {
         itineraryPickUpList: "sr-itinerary-pick-up-list",
         itineraryDropOffContainer: "sr-itinerary-drop-off-container",
         itineraryPickUpContainer: "sr-itinerary-pick-up-container",
+        itineraryDropOffMap: "sr-itinerary-drop-off-map",
+        itineraryPickUpMap: "sr-itinerary-pick-up-map",
         childCareServiceDescriptionTemplate: "sr-child-care-service-description-template",
         childCareServiceDescriptionContainer: "sr-child-care-service-description-container",
     };
+    
+    var addressIconPath = "Content/Images/AddressIcon24.png";
+    var serviceIconPath = "Content/Images/ServiceIcon24.png";
+    var routeIconPath = "Content/Images/RouteIcon24.png";
+
+    var dropOffMap = null;
+    var pickUpMap = null;
 
     var actions = {
         Depart: {
@@ -67,6 +76,7 @@ SmartRoutes.ItineraryPageViewController = function(pageID) {
         CompileTemplates();
     })();
 
+    // Functions for showing the itinerary steps
     function AppendItinerarySteps(node, itinerary) {
         $.each(itinerary.ItineraryActions, function(index, action) {
             var template = null;
@@ -197,7 +207,9 @@ SmartRoutes.ItineraryPageViewController = function(pageID) {
             if (results.Routes && (results.Routes.length > 0)
                 && (index < results.Routes.length) && (index >= 0)) {
                 var routeModel = results.Routes[index];
+                
                 PopulateItineraryList(routeModel);
+                PopulateMaps(routeModel);
 
                 if (results.ChildCares) {
                     PopulateChildCareInfo(results.ChildCares, results.Routes[index].ChildCareIndices);
@@ -205,6 +217,156 @@ SmartRoutes.ItineraryPageViewController = function(pageID) {
             }
         }
     };
+    
+    // Functions for showing the maps
+
+    // get the latitude and longitude from an ILocation
+    function GetLocation(obj) {
+        return [obj.Latitude, obj.Longitude];
+    }
+
+    function GetActionMarker(action) {
+        var iconUrl = "";
+        switch (action.Action) {
+            case actions.Depart.type:
+            case actions.Arrive.type:
+                iconUrl = addressIconPath;
+                break;
+            case actions.DropOff.type:
+            case actions.PickUp.type:
+                iconUrl = serviceIconPath;
+                break;
+            case actions.Board.type:
+            case actions.Exit.type:
+                iconUrl = routeIconPath;
+                break;
+        }
+
+        return L.marker(GetLocation(action), {
+            icon: L.icon({
+                iconUrl: iconUrl,
+                iconSize: L.point(24, 24)
+            }),
+        });
+    }
+
+    function GetActionPopupContent(action) {
+        switch (action.Action) {
+            case actions.Depart.type:
+            case actions.Arrive.type:
+                return action.Address;
+            case actions.DropOff.type:
+            case actions.PickUp.type:
+                return action.ServiceName;
+            case actions.Board.type:
+            case actions.Exit.type:
+                return action.RouteNumber;
+        }
+        return "";
+    }
+
+    function GetActionPolyline(previousAction, currentAction) {
+        var path;
+        if (currentAction.Action == 5) {
+            // create the bus path
+            path = [];
+            for (var j = 0; j < currentAction.RoutePath.length; j++) {
+                path.push(GetLocation(currentAction.RoutePath[j]));
+            };
+
+            // create the polyline
+            return new L.polyline(path, {
+                color: '#f00'
+            });
+        } else {
+            // create the walking path
+            path = [
+                GetLocation(previousAction),
+                GetLocation(currentAction),
+            ]; // create the polyline
+            return new L.polyline(path, {
+                dashArray: '4,6',
+                weight: 3,
+                color: '#00f'
+            });
+        }
+    }
+
+    function InitializeMap(parentId, itineraryActions) {
+        // get the element
+        var mapElement = $("#" + parentId + " .sr-map-canvas")[0];
+        
+        // create the base map
+        var map = L.map(mapElement);
+        var osm = L.tileLayer('http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+            attribution: 'Map data &copy; <a href="http://mapquest.com">MapQuest</a>'
+        });
+        map.addLayer(osm);
+
+        console.log(itineraryActions);
+        
+        // generate the markers and polylines
+        var markers = [];
+        var polylines = [];
+        var allPoints = [];
+        var i;
+        for (i = 0; i < itineraryActions.length; i++) {
+            var previousAction = itineraryActions[i - 1];
+            var currentAction = itineraryActions[i];
+
+            // keep track of all itinery locations
+            allPoints.push(GetLocation(currentAction));
+
+            // generate the marker for this action
+            var marker = GetActionMarker(currentAction);
+            marker.bindPopup(GetActionPopupContent(currentAction));
+            markers.push(marker);
+
+            // generate the polyline for this action
+            if (i > 0) {
+                polylines.push(GetActionPolyline(previousAction, currentAction));
+            }
+        }
+
+        // add the polylines
+        for (i = 0; i < polylines.length; i++) {
+            polylines[i].addTo(map);
+        };
+
+        // add the markers
+        for (i = markers.length - 1; i >= 0; i--) {
+            markers[i].addTo(map);
+        };
+
+        // zoom to show the entire route
+        map.fitBounds(L.polyline(allPoints).getBounds());
+
+        return map;
+    }
+    
+    function PopulateMaps(routeModel) {
+        // Handle the drop off itinerary (if it exists).
+        if (routeModel.DropOffPlan && routeModel.DropOffPlan.ItineraryActions) {
+            $("#" + elementIDs.itineraryDropOffMap).show();
+            if (dropOffMap) {
+                dropOffMap.remove();
+            }
+            dropOffMap = InitializeMap(elementIDs.itineraryDropOffMap, routeModel.DropOffPlan.ItineraryActions);
+        } else {
+            $("#" + elementIDs.itineraryDropOffMap).hide();
+        }
+
+        // Handle the pick up itinerary (if it exists).
+        if (routeModel.PickUpPlan && routeModel.PickUpPlan.ItineraryActions) {
+            $("#" + elementIDs.itineraryPickUpMap).show();
+            if (pickUpMap) {
+                pickUpMap.remove();
+            }
+            pickUpMap = InitializeMap(elementIDs.itineraryPickUpMap, routeModel.PickUpPlan.ItineraryActions);
+        } else {
+            $("#" + elementIDs.itineraryPickUpMap).hide();
+        }
+    }
 
     return {
         // Public:
